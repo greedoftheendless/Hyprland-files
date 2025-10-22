f148d02cef85594fcbaed3a0078e83afcbfb213e
greedoftheendless <greedoftheendless@gmail.com>
2025-10-23 00:16:30 +0530

    nvidia config file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,8 +5,8 @@
 }:
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./nvidia.nix
   ];
 
   # Bootloader.
---
+++ nixos/nvidia.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.modules.nvidia;
+in {
+  options.modules.nvidia = {
+    enable = lib.mkEnableOption "nvidia drivers with prime";
+    withSpecialisation = lib.mkEnableOption "gaming specialization";
+  };
+  config = lib.mkIf cfg.enable {
+    services.xserver = {
+      videoDrivers = ["nvidia"];
+    };
+
+    hardware.nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = true;
+      open = true;
+
+      # TODO: make option
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        amdgpuBusId = "PCI:6:0:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+
+    specialisation = lib.mkIf cfg.withSpecialisation {
+      gaming.configuration = {
+        hardware.nvidia = {
+          powerManagement.finegrained = lib.mkForce false;
+          prime.sync.enable = lib.mkForce true;
+          prime.offload = {
+            enable = lib.mkForce false;
+            enableOffloadCmd = lib.mkForce false;
+          };
+        };
+      };
+    };
+  };
+}
