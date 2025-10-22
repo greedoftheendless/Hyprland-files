a055e1606cc90a2e78d10c5da3c98152121319ab
greedoftheendless <greedoftheendless@gmail.com>
2025-10-23 00:17:19 +0530

    changed garbage collection time

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
               nix.gc = {
                 automatic = true;
                 dates = "daily";
-                options = "--delete-older-than 5d";
+                options = "--delete-older-than 10d";
               };
             })
           ];
